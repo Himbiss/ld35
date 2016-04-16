14a91f59e14ac8fe72f960582c92a4c74f42c422
Vincent Ortland <vincent.ortland@gmx.de>
2016-04-16 14:12:09 +0200

    Added coordinates to entity

--- src/main/java/de/himbiss/ld35/Main.java
+++ src/main/java/de/himbiss/ld35/Main.java
@@ -2,10 +2,6 @@ package de.himbiss.ld35;
 
 import de.himbiss.ld35.engine.Engine;
 import de.himbiss.ld35.world.World;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
 
 /**
  * Created by Vincent on 15.04.2016.
--- src/main/java/de/himbiss/ld35/world/Entity.java
+++ src/main/java/de/himbiss/ld35/world/Entity.java
@@ -4,4 +4,20 @@ package de.himbiss.ld35.world;
  * Created by Vincent on 16.04.2016.
  */
 public class Entity {
+
+    private float coordX;
+    private float coordY;
+
+    public Entity() {
+        this.coordX = 0f;
+        this.coordY = 0f;
+    }
+
+    public float getCoordX() {
+        return coordX;
+    }
+
+    public float getCoordY() {
+        return coordY;
+    }
 }
